72ee42200c6c3eec0ace05610daa7a2a119eea35
HackerSolo1 <eujogominecraftpaulo99@gmail.com>
2026-05-24 01:47:29 -0300

    Feat: adicionado express

--- Backend/script.js
+++ Backend/script.js
@@ -89,3 +89,10 @@ Informações técnicas (ignore isso se não for programador):
 ${error}`);
     }
 });
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log('Servidor online');
+});
