7e573328b007ad6e31dddca73a43df6f03e9f386
HackerSolo1 <eujogominecraftpaulo99@gmail.com>
2026-05-24 01:39:07 -0300

    Feat: adicionado o arquivo inicial (script.js)

---
+++ Backend/script.js
@@ -0,0 +1,91 @@
+const TelegramBot = require('node-telegram-bot-api');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+
+
+const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true });
+
+bot.onText(/\/ajuda/, (msg) => {
+    const helpMessage = `
+Aqui estão os comandos disponíveis:
+Comandos disponíveis:
+/ajuda - Mostra esta mensagem de ajuda
+
+/start - Inicia o bot
+
+/cadastro - Cria um novo cadastro para seu número de telefone
+
+/enviarMensagem - Envia uma mensagem para um número de telefone específico. Insira o ID do usuário e a mensagem após o comando, separados por um espaço. Exemplo: /enviarMensagem 1234567890 Oi, tudo bem?
+`;
+    bot.sendMessage(msg.chat.id, helpMessage);
+});
+
+bot.onText(/\/cadastro/, async (msg) => {
+    const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const userName = msg.from.username || msg.from.first_name;
+    const firstName = msg.from.first_name;
+    const lastName = msg.from.last_name;
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            port: process.env.DB_PORT,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+        const [rows] = await connection.query(`SELECT * FROM users WHERE chat_id = ?`, [chatId]);
+        if (rows.length == 0) {
+            await connection.query(
+                'INSERT INTO users (chat_id, username, first_name, last_name) VALUES (?, ?, ?, ?)',
+                [chatId, userName, firstName, lastName]
+            );
+            bot.sendMessage(chatId, `Cadastro criado com sucesso! Seu ID de usuário é ${userId}.`);
+        } else {
+            bot.sendMessage(chatId, `Usuário já cadastrado com o nome de ${rows[0].first_name}.`)
+        }
+
+    } catch (error) {
+        console.error('Erro ao conectar ao banco de dados:', error);
+        bot.sendMessage(chatId, `Ocorreu um erro ao tentar criar o cadastro.
+
+Informações técnicas (ignore isso se não for programador): 
+${error}`);
+    }
+});
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Bem-vindo, ${msg.from.first_name}! Use /ajuda para ver os comandos disponíveis.`);
+});
+
+bot.onText(/\/enviarMensagem/, async (msg) => {
+    const args = msg.text.split(' ');
+    const chatId = args[1];
+    const message = args.slice(2).join(' ');
+
+    try {
+        const connection = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            port: process.env.DB_PORT,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE
+        });
+        const result = await connection.query(
+            'SELECT * FROM users WHERE chat_id = ?',
+            [chatId]
+        );
+        if (result[0].length > 0) {
+            bot.sendMessage(chatId, message);
+        } else {
+            bot.sendMessage(msg.chat.id, 'Usuário não encontrado.');
+        }
+    } catch (error) {
+        console.error('Erro ao conectar ao banco de dados:', error);
+        bot.sendMessage(msg.chat.id, `Ocorreu um erro ao tentar enviar a mensagem.
+
+Informações técnicas (ignore isso se não for programador): 
+${error}`);
+    }
+});
